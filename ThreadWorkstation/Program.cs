af2b0e0cf3997b72585898c7ba0b7adb79f79a38
GreatLuci <danialabedini1380@gmail.com>
2023-11-22 09:42:36 -0800

    Static Change

--- ThreadWorkstation/Program.cs
+++ ThreadWorkstation/Program.cs
@@ -3,14 +3,14 @@ using System.Threading;
 using System.Threading;
 using System.Threading.Tasks;
 
-public class SharedResource
+public static class SharedResource
 {
-    private int counter=0;
-    private  Mutex mutex = new Mutex();
+    private static int counter=0;
+    private static Mutex mutex = new Mutex();
 
    
 
-    public void Increment(int a)
+    public static void Increment(int a)
     {
         try
         {
@@ -27,7 +27,7 @@ public class SharedResource
         }
     }
 
-    public int show()
+    public static int show()
     {
         return counter;
     }
@@ -39,20 +39,20 @@ class Program
     {
         var tasks1 = new List<Task>();
         var tasks2 = new List<Task>();
-
+        var counter = 0;
         for (int m = 0; m < 10; m++)
         {
-            SharedResource sharedResource1 = new SharedResource();
             Parallel.For(0, 100, i => {
-                tasks1.Add(Task.Run(() => sharedResource1.Increment(1)));
+                tasks1.Add(Task.Run(() => SharedResource.Increment(1)));
+
             });
             Parallel.For(0, 100, i => {
-                tasks2.Add(Task.Run(() => sharedResource1.Increment(1)));
+                tasks2.Add(Task.Run(() => SharedResource.Increment(1)));
             });
             await Task.WhenAll(tasks1);
             await Task.WhenAll(tasks2);
 
-            Console.WriteLine($"Expected Answer: {200} Thread Anwer: {sharedResource1.show()}");
+            Console.WriteLine($"Expected Answer: {counter+=200} Thread Anwer: {SharedResource.show()}");
         }
         Console.ReadLine();
     }
